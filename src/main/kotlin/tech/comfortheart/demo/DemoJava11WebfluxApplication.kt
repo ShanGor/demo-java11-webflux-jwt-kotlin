9fd07fc584d690a81d1ebfd273608dca60a8f2f0
sam <samuelchan@qq.com>
2019-04-07 14:54:39 +0800

    Initial commit

---
+++ src/main/kotlin/tech/comfortheart/demo/DemoJava11WebfluxApplication.kt
@@ -0,0 +1,11 @@
+package tech.comfortheart.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoJava11WebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoJava11WebfluxApplication>(*args)
+}
